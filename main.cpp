5821e39f0126cc4ee9324e6f390eeb00fe5e6449
ddeexxiikk <jakub.batycki.jr@gmail.com>
2021-01-27 19:45:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,47 +2,27 @@
 #include <cstdlib>
 #include <ctime>
 
-using namespace std;
-
-int ktora_tablica(){
-    int liczba=(rand()%3)+1;
-    return liczba;
-}
-
 int main()
 {
     srand(time(NULL));
-
-    string letter[50]={"A","a","B","b","C","C","D","d","E","e","F","f","G","g","H","h","I","i",
+    
+    std::string letnumsym[65]={"A","a","B","b","C","C","D","d","E","e","F","f","G","g","H","h","I","i",
     "J","j","K","k","L","l","M","m","N","n","O","o","P","p","R","r","S","s","T","t","U","u",
-    "V","v","W","w","X","x","Y","y","Z","z"};
-    string number[10]={"0","1","2","3","4","5","6","7","8","9"};
-    string symbol[5]={"!","@","#","$","&"};
+    "V","v","W","w","X","x","Y","y","Z","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$","&"};
 
     int ilosc_znakow;
 
-    cout << "Podaj ilosc znakow: ";
-    cin >> ilosc_znakow;
+    printf("Podaj ilosc znakow: ");
+    std::cin >> ilosc_znakow;
 
-    string * password = new string[ilosc_znakow];
+    std::string * password = new std::string[ilosc_znakow];
     
     for(int i=0; i<ilosc_znakow; i++){
-        int pomoc=ktora_tablica();
-        switch(pomoc){
-        case 1:
-            password[i]=letter[(rand()%50)];
-            break;
-        case 2:
-            password[i]=number[(rand()%10)];
-            break;
-        case 3:
-            password[i]=symbol[(rand()%5)];
-            break;
-        }
+        password[i]=letnumsym[(rand()%65)];
     }
 
     for(int k=0; k<ilosc_znakow; k++)
-        cout << password[k];
+        std::cout << password[k];
 
     delete [] password;
 

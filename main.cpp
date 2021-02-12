7f709b284041b14ee75628f695b40e65e97e1840
ddeexxiikk <jakub.batycki.jr@gmail.com>
2021-02-12 14:13:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 int main()
 {
-    srand(time(NULL));
-    
-    std::string letnumsym[65]={"A","a","B","b","C","C","D","d","E","e","F","f","G","g","H","h","I","i",
+    std::string nazwa, nazwa_aplikacji, letnumsym[65]={"A","a","B","b","C","C","D","d","E","e","F","f","G","g","H","h","I","i",
     "J","j","K","k","L","l","M","m","N","n","O","o","P","p","R","r","S","s","T","t","U","u",
     "V","v","W","w","X","x","Y","y","Z","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$","&"};
-
     int ilosc_znakow;
+
+    printf("Podaj nazwe aplikacji: ");
+    std::cin >> nazwa;
+
+    nazwa_aplikacji= nazwa+".txt";
 
     printf("Podaj ilosc znakow: ");
     std::cin >> ilosc_znakow;
 
     std::string * password = new std::string[ilosc_znakow];
-    
+
+    srand(time(NULL));
+
     for(int i=0; i<ilosc_znakow; i++){
         password[i]=letnumsym[(rand()%65)];
     }
@@ -24,6 +29,13 @@ int main()
     for(int k=0; k<ilosc_znakow; k++)
         std::cout << password[k];
 
+    std::ofstream file;
+    file.open(nazwa_aplikacji, std::ios::app);
+
+    for(int k=0; k<ilosc_znakow; k++)
+        file << password[k];
+
+    file.close();
     delete [] password;
 
     return 0;
